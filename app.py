84410e97a20e14c8978cb36a9fcdb638501a9c63
Marty <martbarc@gmail.com>
2022-06-15 23:49:31 -0400

    App edit

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 #CORS(app) #comment this on deployment
 api = Api(app)
 
-@app.route("/", defaults={'path':''})
+@app.route("/", defaults={'path': ''})
+@app.route('/<path:path>')
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
+
+@app.errorhandler(404)
+def not_found(e):
+  return app.send_static_file('index.html')
 
 api.add_resource(HelloApiHandler, '/flask/hello')
